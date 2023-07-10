bc07b9ba22f273e3cd14576f4984b0b76f801d31
= <=>
2023-07-10 23:16:01 +0200

    Updating return statements for data url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 import pandas as pd
 
 from utils import RESULT_PATH
@@ -7,17 +7,21 @@ app = FastAPI()
 
 @app.get('/')
 async def home():
-
-    message = {'message': 'Welcome to NYSE historic data API. To request data of stocks go to /ticker/[ticker]'}
+    message = {'message': 'Welcome to NYSE historic data API. To request data of stocks go to /data/[ticker]'}
     return Response(content=str(message), media_type='application/json')
 
-@app.get('/ticker/{ticker}')
+@app.get('/credits')
+async def credits():
+    message = {'credits': 'This REST API is a project done by Alejandro Rodriguez & Mahmoud El Bergui'}
+    return Response(content=message, media_type='application/json')
+
+@app.get('/data/{ticker}')
 async def retrieve_data(ticker:str):
     """
-    Search stock history of company using ACT Symbol (ticker) as reference.
+    Search stock history of company using ACT Symbol (ticker) or Company's name as reference.
     """
     ticker = ticker.upper()
-    df = pd.read_csv(RESULT_PATH).query('ticker == @ticker')
+    df = pd.read_csv(RESULT_PATH).query('ticker == @ticker or company == @ticker')
 
     if not df.empty:
         # Split DataFrame into column and data lists
@@ -32,5 +36,5 @@ async def retrieve_data(ticker:str):
 
         return Response(content=cont, media_type='text/csv')
     
-    message = {'message': f'We could not find {ticker} in the NYSE stock list.'}
-    return Response(content=str(message), media_type='application/json')
+    message = f'{ticker} could not be found in the NYSE registry. Please make sure the name is correct.'
+    return HTTPException(status_code=404, detail=message)
